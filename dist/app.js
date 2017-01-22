97c528ae48f5fd62bde7ff043c7c38a482044470
Tim Erwin <tim.erwin@gmail.com>
2017-01-22 01:45:32 -0800

    ...setting up

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+webpackJsonp([0,1],[
+/* 0 */
+/***/ function(module, exports) {
+
+	import React from 'react';
+	import ReactDOM from 'react-dom';
+
+	ReactDOM.render(React.createElement(
+	  'h1',
+	  null,
+	  'Hello, world!'
+	), document.getElementById('content'));
+
+	// $(document).ready(()=> {
+	//   $.get('/data/jobs.json', (results) => {
+	//     console.log(results);
+	//   });
+	// });
+
+/***/ }
+]);

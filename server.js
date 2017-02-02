39784526bed03466bb2cf5c003cb7fd8ad1eb4ef
Tim Erwin <tim.erwin@gmail.com>
2017-02-02 10:24:46 -0800

    port 3000

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const normalizePort = (val) => {
   return false;
 };
 
-const port = normalizePort(process.env.PORT || '80');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
